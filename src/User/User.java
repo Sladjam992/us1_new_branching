6fe3ac628665a23590a38d0077b8eb82e3525b1a
Sladja <sladjamatic92@gmail.com>
2022-08-29 21:11:02 -0400

    new updates od user

--- src/User/User.java
+++ src/User/User.java
@@ -4,5 +4,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("Adding something");
+        System.out.println("Add your name");
     }
 }
