a61e43c3a20a4377b66119689fc159987afba3a7
Sladja <sladjamatic92@gmail.com>
2022-08-29 21:25:29 -0400

    Fixing code

--- src/User/CreditCard.java
+++ src/User/CreditCard.java
@@ -3,6 +3,6 @@ package User;
 public class CreditCard {
     public static void main(String[] args) {
         System.out.println("Inserd your card" );
-        System.out.println("would you like withdarw or deposit?")
+        System.out.println("would you like withdarw or deposit?");
     }
 }
