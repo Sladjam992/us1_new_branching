865d09213ce50f2a5e0a6e983eadf0b2f2312f3a
Sladjam992 <104394505+Sladjam992@users.noreply.github.com>
2022-08-29 21:24:21 -0400

    Update CreditCard.java

--- src/User/CreditCard.java
+++ src/User/CreditCard.java
@@ -3,5 +3,6 @@ package User;
 public class CreditCard {
     public static void main(String[] args) {
         System.out.println("Inserd your card" );
+        System.out.println("would you like withdarw or deposit?")
     }
 }
